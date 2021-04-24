6dfc8416c60e071b09c015890b335e2c723d801b
JoaoRacedo <racedo@uninorte.edu.co>
2021-04-24 12:29:39 -0500

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# arn_seq_pipeline
+# ARN sequential Pipeline
+
+## Introduction
+
+
+## Installation
+Requirements:
+
+- Python 3.6 or greater;
+- Numpy
+
+## Usage
+The script has two main options (MULTIPROCESSING and SINGLE PROCESSOR). The code for the multiprocessing option is commented in the script. To execute the script open a terminal (or command line depending on the OS) and write the following:
+
+For Mac OS and linux
+```bash
+python3 pipeline.py
+```
+For Windows
+```bash
+python pipeline.py
+```
+
+If you want to run the code with different dimensions or with more values change
+```python
+n = np.array([260,20]) # number of rows
+m = np.array([40,20])  # number of columns 
+rho1 = np.array([0.1]) # the value of rho for the first group (or control)
+rho2 = np.array([0.2]) # the value of rho for the second group (or patients)
+b_m = 20    	       # mean value
+```
+
+The script will print in the terminal the results of the missclafication error for all the combinations of the n and m values.
--- pipeline.py
+++ pipeline.py
@@ -2,10 +2,9 @@
 
 import numpy as np
 import math
-from numpy.linalg import multi_dot
+from numpy.linalg import multi_dot 
 np.set_printoptions(suppress=True)
 import multiprocessing as mp
-#print(mp.cpu_count())
 
 def CovMat(X_g):
     [n,m] = X_g.shape
@@ -33,12 +32,11 @@ def Dim_red(L):
 def Eigen(W,V):
     W = W.real
     V = V.real
-    # Sorting in descending order
     idx = W.argsort()[::-1] 
-    W = W[idx] # Diagonal Vector
+    W = W[idx] 
     W_diagonal = np.zeros((len(W),len(W)))
-    np.fill_diagonal(W_diagonal, W) # Convert W into diag matrix
-    V = V[:,idx]  # eigvector columns
+    np.fill_diagonal(W_diagonal, W)
+    V = V[:,idx] 
     return W_diagonal,V
 
 def Statistic(Sc,Sv):
@@ -94,9 +92,7 @@ def Single_Classification(Xc,Xv,xp):
         xp_norm_c = np.dot((xp-bc_dis.T),inv_sigma_c_dis)
         xp_norm_v = np.dot((xp-bv_dis.T),inv_sigma_v_dis)
         Sc_dis = ((nred-1)/nred)*multi_dot([inv_sigma_c_dis,Sigmac,Sc,Sigmac,inv_sigma_c_dis]) + multi_dot([inv_sigma_c_dis,d_bc,d_bc.T,inv_sigma_c_dis])+ (1/nred)*np.dot(xp_norm_c.T,xp_norm_c)
-        #Sc_dis = ((nred-1)/nred)*inv_sigma_c*Sigma_c_dis*Sc*Sigma_c_dis*inv_sigma_c + inv_sigma_c*d_bc*d_bc.T *inv_sigma_c+ (1/nred)*np.dot(xp_norm_c.T,xp_norm_c)
         Sv_dis = ((nred-1)/nred)*multi_dot([inv_sigma_v_dis,Sigmav,Sv,Sigmav,inv_sigma_v_dis]) + multi_dot([inv_sigma_v_dis,d_bv,d_bv.T,inv_sigma_v_dis])+ (1/nred)*np.dot(xp_norm_v.T,xp_norm_v)
-        #Sv_dis = ((nred-1)/nred)*inv_sigma_v*Sigma_v_dis*Sv*Sigma_v_dis*inv_sigma_v + inv_sigma_v*d_bv*d_bv.T *inv_sigma_v+ (1/nred)*np.dot(xp_norm_v.T,xp_norm_v)
         psi_c_inv[i] = 1/(np.abs(Statistic(Sc,Sc_dis)))
         psi_v_inv[i] = 1/(np.abs(Statistic(Sv,Sv_dis)))    
     psi_c_mean = np.mean(psi_c_inv)
@@ -108,68 +104,61 @@ def Single_Classification(Xc,Xv,xp):
 #   MAIN MULTIPROCESSING   #
 ############################
 
-def Test(reps,n,m,rho1,rho2,b_m):
-    b_mean_c = b_m*np.ones(m, dtype = int)
-    b_mean_v = b_mean_c+20
-    I = np.ones((m,1),dtype = int)
-    Sc = (1-rho1)*np.eye(m, dtype=int) + rho1*(I)*I.T
-    Sv = (1-rho2)*np.eye(m, dtype=int) + rho2*(I)*I.T
-    Xc = np.random.multivariate_normal(b_mean_c,Sc,n)
-    Xv = np.random.multivariate_normal(b_mean_v,Sv,n)
-    right_class_c = 0
-    for control in range(n):
-        xp = Xc[control,:]
-        psi_c_mean_single,psi_v_mean_single = Single_Classification(Xc,Xv,xp)
-        if (psi_c_mean_single > psi_v_mean_single):
-            right_class_c += 1
-    MR_c = 100*(1-(right_class_c/n))
-    right_class_v = 0
-    for patient in range(n):
-        xp = Xv[patient,:]
-        psi_c_mean_single,psi_v_mean_single = Single_Classification(Xc,Xv,xp)
-        if (psi_v_mean_single > psi_c_mean_single):
-            right_class_v += 1
-    MR_v = 100*(1-(right_class_v/n))
-    return MR_c+MR_v
+#def Test(reps,n,m,rho1,rho2,b_m):
+#    b_mean_c = b_m*np.ones(m, dtype = int)
+#    b_mean_v = b_mean_c+20
+#    I = np.ones((m,1),dtype = int)
+#    Sc = (1-rho1)*np.eye(m, dtype=int) + rho1*(I)*I.T
+#    Sv = (1-rho2)*np.eye(m, dtype=int) + rho2*(I)*I.T
+#    Xc = np.random.multivariate_normal(b_mean_c,Sc,n)
+#    Xv = np.random.multivariate_normal(b_mean_v,Sv,n)
+#    right_class_c = 0
+#    for control in range(n):
+#        xp = Xc[control,:]
+#        psi_c_mean_single,psi_v_mean_single = Single_Classification(Xc,Xv,xp)
+#        if (psi_c_mean_single > psi_v_mean_single):
+#            right_class_c += 1
+#    MR_c = 100*(1-(right_class_c/n))
+#    right_class_v = 0
+#    for patient in range(n):
+#        xp = Xv[patient,:]
+#        psi_c_mean_single,psi_v_mean_single = Single_Classification(Xc,Xv,xp)
+#        if (psi_v_mean_single > psi_c_mean_single):
+#            right_class_v += 1
+#    MR_v = 100*(1-(right_class_v/n))
+#    return MR_c+MR_v
+#
+#n = np.array([260,20])
+#m = np.array([40,20])
+#rho1 = np.array([0.1])
+#rho2 = np.array([0.2])
+#b_m = 20
+#Matrix = np.array(np.meshgrid(n,m,rho1,rho2)).T.reshape(-1,4)
+#n_reps = 2
+#n_exps = len(n)*len(m)
+#MR_c = np.zeros((n_reps,n_exps))
+#MR_v = np.zeros((n_reps,n_exps))
+#MR_total = np.zeros((n_exps))
+#for k in range(n_exps):
+#    n = Matrix[k,0]
+#    m = Matrix[k,1]
+#    rho1 = Matrix[k,2]
+#    rho2 = Matrix[k,3]
+#    pool = mp.Pool(mp.cpu_count()-1)
+#    result_objects = [pool.apply_async(Test, args=(reps,int(n),int(m),rho1,rho2,b_m)) for reps in range(n_reps)]
+#    pool.close()
+#    temp_results = [result.get() for result in result_objects]
+#    MR_total[k] = np.mean(0.5*np.array(temp_results))
+#
+#MR_total = MR_total[np.newaxis]
+#Results = np.concatenate((Matrix, MR_total.T), axis=1)
+#print(Results)
 
 
-n = np.array([260,20])
-m = np.array([40,20])
-rho1 = np.array([0.1])
-rho2 = np.array([0.2])
-b_m = 20
-Matrix = np.array(np.meshgrid(n,m,rho1,rho2)).T.reshape(-1,4)
-n_reps = 2
-n_exps = len(n)*len(m)
-MR_c = np.zeros((n_reps,n_exps))
-MR_v = np.zeros((n_reps,n_exps))
-MR_total = np.zeros((n_exps))
-for k in range(n_exps):
-    n = Matrix[k,0]
-    m = Matrix[k,1]
-    rho1 = Matrix[k,2]
-    rho2 = Matrix[k,3]
-    pool = mp.Pool(mp.cpu_count()-1)
-    result_objects = [pool.apply_async(Test, args=(reps,int(n),int(m),rho1,rho2,b_m)) for reps in range(n_reps)]
-    pool.close()
-    temp_results = [result.get() for result in result_objects]
-    print(temp_results)
-    MR_total[k] = np.mean(0.5*np.array(temp_results))
-    #for reps in range(n_reps):
-    #    MR_c[reps,k], MR_v[reps,k] = Test(int(n),int(m),rho1,rho2,b_m)
-#pool.join()
-#MR_mean = np.mean(0.5*(MR_c+MR_v),axis = 0)[np.newaxis]
-MR_total = MR_total[np.newaxis]
-Results = np.concatenate((Matrix, MR_total.T), axis=1)
-Results
+################################
+#   MAIN  SINGLE PROCESSOR     #
+################################
 
-
-################
-#   MAIN       #
-################
-
-#n = np.array([20,40,60,80,100,120,140,160,180,200,220,240,260])
-#m = np.array([20,40,60,80,100,120,140])
 n = np.array([260,20])
 m = np.array([40,20])
 rho1 = np.array([0.1])
@@ -194,28 +183,20 @@ for k in range(n_exps):
             Xc = np.random.multivariate_normal(b_mean_c,Sc,n)
             Xv = np.random.multivariate_normal(b_mean_v,Sv,n)
             right_class_c = 0
-            #print("Experiment " + str(k) + " Repetition " + str(reps))
-            #print("Control")
             for control in range(n):
                 xp = Xc[control,:]
                 psi_c_mean_single,psi_v_mean_single = Single_Classification(Xc,Xv,xp)
                 if (psi_c_mean_single > psi_v_mean_single):
                     right_class_c += 1
-            #print(right_class_c)
             MR_c[reps,k] = 100*(1-(right_class_c/n))
-            #print(100*(1-(right_class_c/n)))
-            #print("---------")
             right_class_v = 0
-            #print("Patient")
             for patient in range(n):
                 xp = Xv[patient,:]
                 psi_c_mean_single,psi_v_mean_single = Single_Classification(Xc,Xv,xp)
                 if (psi_v_mean_single > psi_c_mean_single):
                         right_class_v += 1
-            #print(right_class_v)
             MR_v[reps,k] = 100*(1-(right_class_v/n))
-            #print(100*(1-(right_class_v/n)))
-            #print("--------")
+
 MR_mean = np.mean(0.5*(MR_c+MR_v),axis = 0)[np.newaxis]
 Results = np.concatenate((Matrix, MR_mean.T), axis=1)
-Results
+print(Results)
